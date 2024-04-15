94ff755132c597c19a9d97dcde3bd7378d0ec895
Said1224nr <162983493+Said1224nr@users.noreply.github.com>
2024-04-15 15:23:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let Ism = prompt('Ismingizni kiriting')
+
+let Familiya = prompt('Familiyangizni kiriting')
+
+let Yosh = +prompt('Yoshingizni kiriting')
+
+let Fan = prompt('Yoqtirgan Faningizni kiriting')
+
+let Tuman = prompt('Yashaydigan Tumaningizni kiriting')
+
+
+console.log(`Foydalanuvchi ismi:${Ism}\nFoydalanuvchi familiyasi:${Familiya}\nFoydalanuvchi yoshi:${Yosh}\nYoqtirgan fani:${Fan}\nYashayotgan Tumani:${Tuman}`);
